a77ef6871cb69ccded1bd790578f22b1bee4819d
rapidliner0 <rapidliner.express+git@gmail.com>
2020-10-24 07:26:26 +0000

    Improve: move equals() method: Dollar -> Money

--- money/src/main/java/money/Dollar.java
+++ money/src/main/java/money/Dollar.java
@@ -7,8 +7,4 @@ public class Dollar extends Money {
   Dollar times(int multiplier) {
     return new Dollar(amount * multiplier);
   }
-  public boolean equals(Object object) {
-    Dollar dollar = (Dollar) object;
-    return amount == dollar.amount;
-  }
 }
