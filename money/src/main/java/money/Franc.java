f875b1cbd749283e4c412dc818e31bcc83ec2391
rapidliner0 <rapidliner.express+git@gmail.com>
2020-10-24 07:41:22 +0000

    Improve: remove method equals()

--- money/src/main/java/money/Franc.java
+++ money/src/main/java/money/Franc.java
@@ -7,13 +7,4 @@ public class Franc extends Money{
   Franc times(int multiplier) {
     return new Franc(amount * multiplier);
   }
-  public boolean equals(Object object) {
-<<<<<<< HEAD
-    Franc franc = (Franc) object;
-    return amount == franc.amount;
-=======
-    Money money = (Money) object;
-    return amount == money.amount;
->>>>>>> 90b81c8... Improve: variable 'dollar' -> 'money'
-  }
 }
