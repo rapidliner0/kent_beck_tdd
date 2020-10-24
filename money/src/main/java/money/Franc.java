e29c23fb1b5a5868ee0589cfb644c1ff107af679
rapidliner0 <rapidliner.express+git@gmail.com>
2020-10-24 07:40:30 +0000

    Improve: variable 'franc' -> 'money'

--- money/src/main/java/money/Franc.java
+++ money/src/main/java/money/Franc.java
@@ -8,7 +8,12 @@ public class Franc extends Money{
     return new Franc(amount * multiplier);
   }
   public boolean equals(Object object) {
+<<<<<<< HEAD
     Franc franc = (Franc) object;
     return amount == franc.amount;
+=======
+    Money money = (Money) object;
+    return amount == money.amount;
+>>>>>>> 90b81c8... Improve: variable 'dollar' -> 'money'
   }
 }
