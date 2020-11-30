f0cbdfa11df805bfb0621f204c7e64bd2ff1c97a
rapidliner0 <rapidliner.express+git@gmail.com>
2020-11-30 13:19:04 +0000

    Add: class Money: Money()

--- money/src/main/java/money/Franc.java
+++ money/src/main/java/money/Franc.java
@@ -2,8 +2,7 @@ package money;
 
 public class Franc extends Money{
   Franc(int amount, String currency) {
-    this.amount = amount;
-    this.currency = "CHF";
+    super(amount, currency);
   }
   Money times(int multiplier) {
     return Money.franc(amount * multiplier);
